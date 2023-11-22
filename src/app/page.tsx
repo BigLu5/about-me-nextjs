d5ceeb6d16ebe608b7665dc4f136e820e5e2332c
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-11-22 16:12:58 +0000

    add image and fix header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h2 style={{ textAlign: "center" }}>About Luke Reynolds</h2>
       <div className="relative">
         <Image
           src="/game-banner.png"
           alt="Game Banner"
-          layout="fill"
-          objectFit="cover"
+          width={1152}
+          height={300}
         />
       </div>
-      <h2 style={{ textAlign: "center" }}>About Luke Reynolds</h2>
       <p style={{ textAlign: "center" }}>
         He once buttered bread before putting it in the toaster but has come a
         long way since then.
       </p>
-      <p>
+      <p style={{ textAlign: "center" }}>
         Here is a quote to live by: "An elephant never forgets, but I forget
         what the elephant remembered."
       </p>
